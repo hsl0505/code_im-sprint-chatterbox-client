6d528204b99bdd0a7f704bbc5a45e0c4fdc5be56
정진석 <jin3378s@gmail.com>
2019-11-27 16:38:56 +0900

    test student.json

--- __test__/student.js
+++ __test__/student.js
@@ -1,16 +1,15 @@
-const studentData = require("../student.json");
-var chai = require('chai')
-var expect = chai.expect
+const studentData = require('../student.json');
+var chai = require('chai');
+var expect = chai.expect;
 
-describe("student", () => {
-  it("put correct name on student.json", () => {
-    const rawName =
-      "스프린트를 진행하는 수강생분의 이름을 한글로! 적어주세요! 예)존도우";
+describe('student', () => {
+  it('put correct name on student.json', () => {
+    const rawName = '성함을 입력 해주세요';
     expect(studentData.student).not.to.equal(rawName);
   });
 
-  it("put correct class on student.json", () => {
-    const rawClass = "기수를 숫자만! 입력해주세요! 예)10";
+  it('put correct class on student.json', () => {
+    const rawClass = '기수를 입력 해주세요 ex) 15';
     expect(studentData.theClass).not.to.equal(rawClass);
   });
 });
